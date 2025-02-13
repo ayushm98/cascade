41216274671d87d370ca741fda01b642e02b44c7
ayushm98 <ayushkumarmalik10@gmail.com>
2025-02-13 11:20:00 -0600

    refactor: export heuristics from router module

--- src/cascade/router/__init__.py
+++ src/cascade/router/__init__.py
@@ -1,6 +1,7 @@
 """Router module for complexity classification and model routing."""
 
 from cascade.router.classifier import ONNXComplexityClassifier
+from cascade.router.heuristics import classify_by_heuristics
 from cascade.router.routing_engine import RoutingDecision, RoutingEngine, route_query
 
 __all__ = [
@@ -8,4 +9,5 @@ __all__ = [
     "RoutingEngine",
     "RoutingDecision",
     "route_query",
+    "classify_by_heuristics",
 ]
